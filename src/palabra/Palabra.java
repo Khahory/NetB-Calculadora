432b7714287b548d2ad71fe4ad104543798ab0c1
KhahoryITLA <20164247@itla.edu.do>
2019-06-04 00:16:56 -0400

    Commit

---
+++ src/palabra/Palabra.java
@@ -0,0 +1,21 @@
+
+package palabra;
+
+
+public class Palabra {
+   public static void main(String[] args){
+       String a = "AntoniO";
+                 //0123456
+                 
+       String b;
+       
+       b = a.substring(a.length()-2 ); //+ a.substring(6);
+       
+       System.out.println(b);
+       
+       
+       
+       System.out.println("----------------------------");
+       System.out.println(a);
+   } 
+}
